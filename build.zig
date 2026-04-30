d82efdf258e49650b347bcd83e43267039d00138
kingye <kingye@petalmail.com>
2026-04-30 11:19:20 +0800

    feat: step 7 - 报表生成 report.zig (libxlsxwriter 明细表+汇总表)

--- build.zig
+++ build.zig
@@ -21,6 +21,58 @@ pub fn build(b: *std.Build) void {
     sqlite_dep.linkLibC();
     b.installArtifact(sqlite_dep);
 
+    const xlsxwriter_dep = b.addStaticLibrary(.{
+        .name = "xlsxwriter",
+        .target = target,
+        .optimize = optimize,
+    });
+    xlsxwriter_dep.addCSourceFiles(.{
+        .files = &.{
+            "libs/xlsxwriter_src/app.c",
+            "libs/xlsxwriter_src/chart.c",
+            "libs/xlsxwriter_src/chartsheet.c",
+            "libs/xlsxwriter_src/comment.c",
+            "libs/xlsxwriter_src/content_types.c",
+            "libs/xlsxwriter_src/core.c",
+            "libs/xlsxwriter_src/custom.c",
+            "libs/xlsxwriter_src/drawing.c",
+            "libs/xlsxwriter_src/format.c",
+            "libs/xlsxwriter_src/hash_table.c",
+            "libs/xlsxwriter_src/metadata.c",
+            "libs/xlsxwriter_src/packager.c",
+            "libs/xlsxwriter_src/relationships.c",
+            "libs/xlsxwriter_src/rich_value.c",
+            "libs/xlsxwriter_src/rich_value_rel.c",
+            "libs/xlsxwriter_src/rich_value_structure.c",
+            "libs/xlsxwriter_src/rich_value_types.c",
+            "libs/xlsxwriter_src/shared_strings.c",
+            "libs/xlsxwriter_src/styles.c",
+            "libs/xlsxwriter_src/table.c",
+            "libs/xlsxwriter_src/theme.c",
+            "libs/xlsxwriter_src/utility.c",
+            "libs/xlsxwriter_src/vml.c",
+            "libs/xlsxwriter_src/workbook.c",
+            "libs/xlsxwriter_src/worksheet.c",
+            "libs/xlsxwriter_src/xmlwriter.c",
+            "libs/xlsxwriter_third_party/dtoa/emyg_dtoa.c",
+            "libs/xlsxwriter_third_party/md5/md5.c",
+            "libs/xlsxwriter_third_party/minizip/ioapi.c",
+            "libs/xlsxwriter_third_party/minizip/zip.c",
+            "libs/xlsxwriter_third_party/tmpfileplus/tmpfileplus.c",
+        },
+        .flags = &.{
+            "-DNOCRYPT",
+            "-DUSE_MINIZIP",
+            "-DSTANDARD_LICENSE",
+        },
+    });
+    xlsxwriter_dep.addIncludePath(b.path("libs/xlsxwriter"));
+    xlsxwriter_dep.addIncludePath(b.path("libs/xlsxwriter_src"));
+    xlsxwriter_dep.addIncludePath(b.path("libs/xlsxwriter_third_party"));
+    xlsxwriter_dep.addIncludePath(b.path("libs/xlsxwriter_third_party/minizip"));
+    xlsxwriter_dep.linkLibC();
+    b.installArtifact(xlsxwriter_dep);
+
     const exe = b.addExecutable(.{
         .name = "invoice",
         .root_source_file = b.path("src/main.zig"),
@@ -28,7 +80,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe.addIncludePath(b.path("libs"));
+    exe.addIncludePath(b.path("libs/xlsxwriter"));
     exe.linkLibrary(sqlite_dep);
+    exe.linkLibrary(xlsxwriter_dep);
     exe.linkLibC();
     b.installArtifact(exe);
 
@@ -46,7 +100,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe_tests.addIncludePath(b.path("libs"));
+    exe_tests.addIncludePath(b.path("libs/xlsxwriter"));
     exe_tests.linkLibrary(sqlite_dep);
+    exe_tests.linkLibrary(xlsxwriter_dep);
     exe_tests.linkLibC();
 
     const run_exe_tests = b.addRunArtifact(exe_tests);
